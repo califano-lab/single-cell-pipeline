897d3be7a0ea03aafbb372d77645d7c9d8c3fa2c
hd2326 <hd2326@columbia.edu>
2018-08-09 23:19:47 -0400

    checkjobs.r

--- Modules/ARACNe/checkjobs.r
+++ Modules/ARACNe/checkjobs.r
@@ -14,12 +14,22 @@ tmp <- sapply(fn, function(x) {
     tmp[length(tmp)]
 })
 names(tmp) <- sapply(strsplit(fn, "_"), function(x) sub(".log", "", x[length(x)]))
+names(tmp)[which(names(tmp) == "all")] <- 0#ar_acro-reg_all.log
 tmp <- as.numeric(names(tmp))[-grep("Total time elapsed", tmp)]
 if (length(tmp)>0) {
     for (i in tmp) {
-        qlog <- paste(wd, "/ar_", acro, "-", reg, "_", i, ".log", sep="")
-        command <- paste(java, " -Xmx16000M -jar ", ad, "/aracne.jar", " -e ", wd, "/", acro, "-expmat.dat -o ", wd, "/", acro, "-", reg, "/ --tfs ", rfn, " --pvalue 0.00000001 --threads 4 --seed ", i, sep="")
-        system(paste("echo \"", command, "\" | qsub -l mem=20G,time=1:: -N ar_", acro, "-", reg, " -j yes -o ", qlog, " -cwd", sep=""))
+        if (i == 0){
+            message("Re-submitting no-bootstrap no-dpi network")
+            qlog <- paste(wd, "/ar_", acro, "-", reg, "_all.log", sep="")
+            command <- paste(java, " -Xmx36000M -jar ", ad, "/aracne.jar", " -e ", wd, "/", acro, "-expmat.dat -o ", wd, "/", acro, "-", reg, "_all/ --tfs ", rfn, " --pvalue 1 --threads 4 --nodpi --nobootstrap", sep="")
+            system(paste("echo \"", command, "\" | qsub -l mem=40G,time=2:: -N ar_", acro, "-", reg, " -j yes -o ", qlog, " -cwd", sep=""))
+            
+        }else{
+            message(paste("Re-submitting bootstrap network #", i, sep = ""))
+            qlog <- paste(wd, "/ar_", acro, "-", reg, "_", i, ".log", sep="")
+            command <- paste(java, " -Xmx16000M -jar ", ad, "/aracne.jar", " -e ", wd, "/", acro, "-expmat.dat -o ", wd, "/", acro, "-", reg, "/ --tfs ", rfn, " --pvalue 0.00000001 --threads 4 --seed ", i, sep="")
+            system(paste("echo \"", command, "\" | qsub -l mem=20G,time=1:: -N ar_", acro, "-", reg, " -j yes -o ", qlog, " -cwd", sep=""))
+        }
     }
     qlog <- paste(wd, "/chk_", acro, "-", reg, ".log", sep="")
     command <- paste(rscript, " ", ad, "checkjobs.r ", wd, " ", acro, " ", reg, " ", rfn, " ", ad, " ", rscript, " ", java, " ", sep="")
